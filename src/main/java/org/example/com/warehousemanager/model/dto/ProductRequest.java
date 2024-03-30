d23e8b04c721fd7bd1da354c983717eff26f8e24
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-03-30 11:23:28 +0400

    feat: added swagger documentation

--- src/main/java/org/example/com/warehousemanager/model/dto/ProductRequest.java
+++ src/main/java/org/example/com/warehousemanager/model/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package org.example.com.warehousemanager.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
@@ -11,20 +12,26 @@ import java.math.BigDecimal;
 @Setter
 public class ProductRequest {
     @NotBlank(message="name cannot be blank")
+    @Schema(description = "Product name")
     private String name;
 
     @NotBlank(message="article cannot be blank")
+    @Schema(description = "Product article. Is unique for each product")
     private String article;
 
     @NotBlank(message="description cannot be blank")
+    @Schema(description = "Product description")
     private String description;
 
     @Positive(message="Price should be greater than 0")
+    @Schema(description = "Product price in dollars. Should be greater than 0", example = "1.00")
     private BigDecimal price;
 
-    @Positive(message="Price should be greater than 0")
+    @Positive(message="Price should be greater than 0. Should be greater than 0")
+    @Schema(description = "Count of this product", example = "1")
     private Integer count;
 
     @NotBlank(message="category cannot be blank")
+    @Schema(description = "Product category")
     private String category;
 }
